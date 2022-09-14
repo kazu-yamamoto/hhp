f1b5ad8e2bf11bdd6e6995a02f8ad71853f38fef
Kazu Yamamoto <kazu@iij.ad.jp>
2022-09-14 14:02:49 +0900

    fixing expand

--- lib/Hhp/Logger.hs
+++ lib/Hhp/Logger.hs
@@ -11,8 +11,8 @@ import GHC (Ghc, DynFlags(..), SrcSpan(..))
 import qualified GHC as G
 import GHC.Data.Bag (bagToList)
 import GHC.Data.FastString (unpackFS)
-import GHC.Driver.Session (dopt, DumpFlag(Opt_D_dump_splices), initSDocContext)
-import GHC.Utils.Error (Severity(..), errMsgSpan)
+import GHC.Driver.Session (initSDocContext)
+import GHC.Utils.Error (Severity(..)) -- errMsgSpan
 import GHC.Utils.Monad (liftIO)
 import GHC.Utils.Outputable (SDoc, SDocContext)
 
@@ -20,6 +20,7 @@ import GHC.Utils.Outputable (SDoc, SDocContext)
 import GHC.Utils.Error (MessageClass(..))
 import GHC.Utils.Logger (LogFlags(..))
 #else
+import GHC.Driver.Session (dopt, DumpFlag(Opt_D_dump_splices))
 import Hhp.Doc (styleUnqualified)
 #endif
 
@@ -36,8 +37,7 @@ import Hhp.Types (Options(..), convert)
 
 ----------------------------------------------------------------
 
--- type LogInfo = (DynFlags,Severity,SrcSpan,SDoc)
-type LogInfo = (SDocContext,Severity,SrcSpan,SDoc)
+type LogInfo = (Bool,SDocContext,Severity,SrcSpan,SDoc)
 
 newtype LogRef = LogRef (IORef ([LogInfo] -> [LogInfo]))
 
@@ -55,14 +55,16 @@ readAndClearLogRef opt (LogRef ref) = do
 appendLogRef :: LogRef -> LogAction
 #if __GLASGOW_HASKELL__ >= 904
 appendLogRef (LogRef ref) flag mc src msg = do
-    let sev = case mc of
-          MCDiagnostic sev0 _ -> sev0
+    let (dump,sev) = case mc of
+          MCDiagnostic sev0 _ -> (False, sev0)
+          _                   -> (True,  SevError) -- dummy
         ctx = log_default_user_context flag
-        !l = (ctx, sev, src, msg)
+        !l = (dump, ctx, sev, src, msg)
 #else
 appendLogRef (LogRef ref) flag _ sev src msg = do
     let ctx = initSDocContext flag styleUnqualified
-        !l = (ctx, sev, src, msg)
+        dump = isDumpSplices flag
+        !l = (dump, ctx, sev, src, msg)
 #endif
     modifyIORef ref (\b -> b . (l:))
 
@@ -94,17 +96,17 @@ sourceError opt err = do
     return (Left ret)
 
 errBagToStrList :: SDocContext -> ErrorMessages -> [String]
-errBagToStrList ctx = map ppErrMsg  . reverse . bagToList . getMessages
+errBagToStrList ctx = map ppErrMsg . reverse . bagToList . getMessages
   where
     ppErrMsg err = showPage ctx msg
        where
-         spn = errMsgSpan err
+--         spn = errMsgSpan err
          msg = pprLocErrMessage err
 
-ppMsg :: (SDocContext, Severity, SrcSpan, SDoc) -> String
-ppMsg (ctx,sev,spn,msg) = prefix ++ cts
+ppMsg :: (Bool, SDocContext, Severity, SrcSpan, SDoc) -> String
+ppMsg (True,ctx,_   ,_ ,msg) =           showPage ctx msg
+ppMsg (_,   ctx,sev,spn,msg) = prefix ++ showPage ctx msg
   where
-    cts = showPage ctx msg
     prefix = fromMaybe checkErrorPrefix $ do
         (line,col,_,_) <- getSrcSpan spn
         file <- normalise <$> getSrcFile spn
@@ -124,8 +126,10 @@ getSrcFile :: SrcSpan -> Maybe String
 getSrcFile (G.RealSrcSpan spn _) = Just . unpackFS . G.srcSpanFile $ spn
 getSrcFile _                     = Nothing
 
+#if __GLASGOW_HASKELL__ < 904
 isDumpSplices :: DynFlags -> Bool
 isDumpSplices dflag = dopt Opt_D_dump_splices dflag
+#endif
 
 getSrcSpan :: SrcSpan -> Maybe (Int,Int,Int,Int)
 getSrcSpan (RealSrcSpan spn _) = Just ( G.srcSpanStartLine spn
